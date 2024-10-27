8aec11326e592ac49c709d3acadd226b7d2a2413
bnithin06 <bnithin2822@gmail.com>
2024-10-27 19:18:38 +0530

    first commit

---
+++ mediaqueries/index.js
@@ -0,0 +1,6 @@
+const bar=document.getElementById('bar');
+const nav_links=document.getElementById('nav_links');
+// console.log(bar);
+bar.addEventListener('click',()=>{
+    console.log('clicked');
+})
